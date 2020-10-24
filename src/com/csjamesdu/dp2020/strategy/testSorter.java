da7a0ed76d036c0e4e1d02a029b0bc1c7ec055a2
YU(James) DU <csjamesdu@gmail.com>
2020-10-24 21:39:46 +1100

    test

--- src/com/csjamesdu/dp2020/strategy/testSorter.java
+++ src/com/csjamesdu/dp2020/strategy/testSorter.java
@@ -16,6 +16,8 @@ public class testSorter {
                 if(o1.getHeight()<o2.getHeight()) return -1;
                 else if(o1.getHeight() > o2.getHeight()) return 1;
                 else return 0;
+
+
             }
         });
 
